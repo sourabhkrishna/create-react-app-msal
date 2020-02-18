bea2c5b6174056c6fcb77adb1fc7fa311fa52399
sourabhkrishna <sourabh26@gmail.com>
2020-02-18 11:47:03 +0530

    Initial Commit

---
+++ src/auth.js
@@ -0,0 +1,28 @@
+import { MsalAuthProvider, LoginType } from 'react-aad-msal';
+ 
+// Msal Configurations
+const config = {
+  auth: {
+    authority: 'https://login.microsoftonline.com/{Your Azure TenantID}',
+    clientId: 'Your Azure Client ID',
+  },
+  cache: {
+    cacheLocation: "localStorage",
+    storeAuthStateInCookie: false
+  }
+};
+ 
+// Authentication Parameters
+const authenticationParameters = {
+  scopes: [
+    'user.read'
+  ]
+}
+ 
+// Options
+const options = {
+  loginType: LoginType.Redirect,
+  tokenRefreshUri: window.location.origin + '/auth.html'
+}
+ 
+export const authProvider = new MsalAuthProvider(config, authenticationParameters, options)
